191c71a50632ade3c3303a2a784d5a72068a686b
Abhisheksharma004 <abhishekkumarranjan965@gmail.com>
2024-09-23 01:10:12 +0530

    roust methods

--- routes/web.php
+++ routes/web.php
@@ -6,11 +6,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::get('/home/{id?}', function (string $id = null) {
+    if($id){
+        return "<h1>Post Id : " .$id. "</h1>";
+    }else{
+        return "<h1>NO ID Found</h1>";
+    }
 });
 
-// Route::view('home','/home');
+// Route::view('/home','home');
 
 // Route::get('/new', function () {
 //     return view('home');
